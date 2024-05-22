4b8e7ba4abe7cfe29962612ac411e3f750ec5b2b
tj <thinupavani@gmail.com>
2024-05-22 08:53:33 +0530

    sec two button styles added

--- components/Buttons/index.tsx
+++ components/Buttons/index.tsx
@@ -27,7 +27,7 @@ export const ButtonHero: React.FC<ButtonPrimaryProps> = ({ link, title, id }) =>
 
   export const ButtonPrimaryArrow: React.FC<ButtonPrimaryProps> = ({ link, title, id }) => {
     return (
-      <Link key={id} className="navButton" href={link} style={{padding: '10px 50px !important;'}}>
+      <Link key={id} className="arrowButtonRedBg" href={link} style={{padding: '10px 20px !important;'}}>
         {title}
         <FaChevronRight />
       </Link>
